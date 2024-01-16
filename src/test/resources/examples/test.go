7b809bfbb1242a6065a4ed4d8d28b2e4df953cd6
linchudong <linchudong@zhikan.net.cn>
2024-01-16 11:54:08 +0800

    test

---
+++ src/test/resources/examples/test.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// This is a comment
+/**
+	another comment
+**/
+
+// Msg function
+func Msg() {
+	/* This is a single line comment */
+}
+
+func main() {
+	fmt.Println("hw")
+}
+
+// This is a comment
