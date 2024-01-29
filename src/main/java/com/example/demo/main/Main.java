4f4e21538d03634b443b812e6d221cbac8de6558
linchudong <linchudong@zhikan.net.cn>
2024-01-29 16:56:58 +0800

    测试

--- src/main/java/com/example/demo/main/Main.java
+++ src/main/java/com/example/demo/main/Main.java
@@ -5,6 +5,7 @@ import java.text.MessageFormat;
 public class Main {
 
     public static void main(String[] args) {
+        // 测试
         String url = "/openapi-doc/manual/di-san-fang-ji-cheng/fei-shu-ji-cheng/shi-yong-fang-fa.html?source=feishu&type=all_desc";
         String host = "https://ligai.cn";
         format(url, host);
