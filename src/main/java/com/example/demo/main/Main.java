2e910b78ed34ca5c8d35b69b152d46281bcf06c8
linchudong <linchudong@zhikan.net.cn>
2024-01-30 15:32:08 +0800

    添加无用注释

--- src/main/java/com/example/demo/main/Main.java
+++ src/main/java/com/example/demo/main/Main.java
@@ -5,9 +5,11 @@ import java.text.MessageFormat;
 public class Main {
 
     public static void main(String[] args) {
+        // 加一个没用到注释
         String url = "/openapi-doc/manual/di-san-fang-ji-cheng/fei-shu-ji-cheng/shi-yong-fang-fa.html?source=feishu&type=all_desc";
         String host = "https://ligai.cn";
         format(url, host);
+        // 这里也加一个没用到注释
     }
 
     public static void format(String url, String... args) {
