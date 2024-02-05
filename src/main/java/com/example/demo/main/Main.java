2d64f4d0efc88af6803e84138573d4de343cc1e5
linchudong <linchudong@zhikan.net.cn>
2024-02-05 11:05:48 +0800

    区间合并

--- src/main/java/com/example/demo/main/Main.java
+++ src/main/java/com/example/demo/main/Main.java
@@ -1,13 +1,16 @@
 package com.example.demo.main;
 
+import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         List<Integer> result = numbers.stream()
@@ -17,6 +20,10 @@ public class Main {
                 .collect(Collectors.toList());
 
         System.out.println(result);
+
+        // 区间合并
+        int[][] merge = merge(new int[][]{{1, 4}, {0, 2}, {3, 5}, {5, 7}, {8, 10}, {15, 18}});
+        System.out.println(Arrays.deepToString(merge));
     }
 
     public static void format(String url, String... args) {
@@ -26,5 +33,23 @@ public class Main {
         // 随便提交一个注释
         // 再提交一个注释
         // 222
+    }
+
+    public static int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        List<int[]> ans = new ArrayList<>();
+        for (int[] interval : intervals) {
+            if (interval[0] <= end) {
+                end = Math.max(end, interval[1]);
+            } else {
+                ans.add(new int[]{start, end});
+                start = interval[0];
+                end = interval[1];
+            }
+        }
+        ans.add(new int[]{start, end});
+        return ans.toArray(new int[ans.size()][]);
     }
 }
